e6fdb636b2a961dba507264b61f4fa35fa024d43
David Lawrence <davidl@jlab.org>
2021-03-04 14:31:59 -0500

    Realign with the csh version and update to fix module issues

--- setup/spack_env.sh
+++ setup/spack_env.sh
@@ -15,63 +15,117 @@
 
 # Usage
 function spack_env_usage () {
+	echo ""
 	echo "Usage:"
 	echo ""
-	echo "source spack_env.sh [os release [compiler]]"
+	echo "source spack_env.csh [os=OS/RELEASE] [lmod [modules]]"
 	echo ""
 	echo "This script should be sourced to set up your shell"
-	echo "environment to use a spack installation from CVMFS."
-	echo "It should normally be run with no arguments and it"
-	echo "determine the OS/Release automatically by running the"
-	echo "lsb_release program. You may also specify the OS and"
-	echo "Release values by passing them as the first two "
-	echo "positional arguments of the script."
+	echo "environment to use a spack installation from /cvmfs."
+	echo "It should normally be run with no arguments or just"
+	echo "lmod. One may optionally specify addtional modules"
+	echo "to load."
 	echo ""
-	echo "e.g."
+	echo "modules"
+	echo "==========================="
+	echo "Additional modules may be specified at the end of the"
+	echo "arguments list. These will be loaded using the"
+	echo "module load command. If one specifies the special"
+	echo "lmod module then that will be loaded via spack load"
+	echo "and the environment modified to use the more user friendly"
+	echo "heirachical module system available in the spack system."
 	echo ""
-	echo "  source /cvmfs/oasis.opensciencegrid.org/jlab/epsci/spack_env.sh centos 7.7.1908"
+	echo "NOTE: while specifying lmod is recommended, it will"
+	echo "cause this script about 2 seconds longer to execute!"
 	echo ""
-	echo "The valid values for os and release can be found by"
-	echo "looking at the subdirectories of the directory"
-	echo "/cvmfs/oasis.opensciencegrid.org/jlab/epsci ."
+	echo "Module names will be different depending on whether you"
+	echo "use the default JLab lmod or the one loaded from spack."
+	echo "The lmod module should be specified first if you specify"
+	echo "additional module names using the more user-friendly"
+	echo "naming scheme (i.e. package/version)"
+	echo ""
+	echo "compiler"
+	echo "==========================="
+	echo "You can specify a compiler to load by doing something like"
+	echo "this:"
+	echo ""
+	echo "   source /cvmfs/oasis.opensciencegrid.org/jlab/epsci/spack_env.sh lmod gcc/9.3.0"
 	echo ""
 	echo "This will configure your shell environment so that"
-	echo "you can use the lmod module system. Heirarchical"
+	echo "you can use the lmod heirarchical module system. Heirarchical"
 	echo "modules are enabled so only modules relevant for the"
 	echo "currently configured compiler will be shown by default."
-	echo "For example, if you wish to use the gcc 9.3.0 compiler"
+	echo "For example, if you wish to use the gcc 8.3.1 compiler"
 	echo "load it with:"
 	echo ""
-	echo "     module load gcc/9.3.0"
+	echo "     module load gcc/8.3.1"
 	echo ""
 	echo "Then check what packages are available with:"
 	echo ""
 	echo "     module avail"
 	echo ""
-	echo "You may also specify the compiler when sourcing this"
-	echo "script by giving it as a 3rd argument. In this case"
-	echo "you will need to also give the os and release arguments."
+	echo "os/release"
+	echo "==========================="
+	echo "This script will determine the OS/RELEASE value"
+	echo "automatically by running the lsb_release program. You"
+	echo "may also specify this using the os=XXX/YYY option."
+	echo ""
+	echo "e.g."
+	echo ""
+	echo "  source /cvmfs/oasis.opensciencegrid.org/jlab/epsci/spack_env.sh os=centos/7.7.1908"
+	echo ""
+	echo "The valid values for os and release can be found by"
+	echo "looking at the subdirectories of the directory"
+	echo "/cvmfs/oasis.opensciencegrid.org/jlab/epsci ."
+	echo ""
+	echo "Please direct any questions to:"
+	echo "   davidl@jlab.org"
 	echo ""
 }
 
-# Print usage statement and exit if -h option is given
-#foreach s ( $* ); if ( $s == '-h' ) then ; spack_env_usage ; endif ; end
+# Loop over command line arguments
+os=""
+ver=""
+modules=""
 for s  in "$@" ; do
 	if [ $s == '-h' ] ; then
+		# Help
 		spack_env_usage
 		return
+	elif [[ "$s" == os=* ]] ; then
+		# OS/Release
+		os=`echo $s | cut -d '=' -f2 | cut -d '/' -f1`
+		ver=`echo $s | cut -d '=' -f2 | cut -d '/' -f2`
+		[ "$ver" == "$os" ] && ver=''
+	else
+		# Module
+		modules="$modules $s"
 	fi
 done
 
+# If user did not specify os name and release then find it
+[ "$os"  == "" ] && os=`lsb_release -i -s`
+[ "$ver" == "" ] && ver=`lsb_release -r -s`
+
+
+# Print usage statement and exit if -h option is given
+#foreach s ( $* ); if ( $s == '-h' ) then ; spack_env_usage ; endif ; end
+#for s  in "$@" ; do
+#	if [ $s == '-h' ] ; then
+#		spack_env_usage
+#		return
+#	fi
+#done
+
 
 # Get os name and release
-if [ "$#" -ge 2 ] ; then
-	os=$1
-	ver=$2
-else
-	os=$(lsb_release -i -s)
-	ver=$(lsb_release -r -s)
-fi
+#if [ "$#" -ge 2 ] ; then
+#	os=$1
+#	ver=$2
+#else
+#	os=$(lsb_release -i -s)
+#	ver=$(lsb_release -r -s)
+#fi
 
 # Coherce os name/release to match installed versions
 os=$(echo "$os" | tr '[A-Z]' '[a-z]')  # make lowercase
@@ -89,29 +143,59 @@ esac
 setupfile="/cvmfs/oasis.opensciencegrid.org/jlab/epsci/${os}/${ver}/share/spack/setup-env.sh"
 if [ -f ${setupfile} ] ; then
 
-	# Source the SPACK setup-env.sh file
+
+	# This is left here for symmetry with the csh version of this
+	# script. It does not look like the bash version of the 
+	# spack setup-env.sh script does the same contortions to 
+	# get at the SPACK_ROOT value.
+	#
+	export SPACK_ROOT="/cvmfs/oasis.opensciencegrid.org/jlab/epsci/${os}/${ver}"
+
+	# Source the SPACK setup-env.csh file
 	cmd="source ${setupfile}"
-	echo $cmd
+	[[ $- == *i* ]] && echo $cmd
 	$cmd
+	
+	# Loop over modules user wants automatically loaded
+	for  mod in $modules ; do
 
-	spack load lmod
-	source $(spack location -i lmod)/lmod/lmod/init/bash
-	spackarch=$(spack arch)
-	spackgenarch=$(spack arch -p)-$(spack arch -o)-x86_64
-	module unuse ${SPACK_ROOT}/share/spack/modules/${spackarch}
-	module unuse ${SPACK_ROOT}/share/spack/modules/${spackgenarch}
-	module use ${SPACK_ROOT}/share/spack/lmod/${spackgenarch}/Core
+		# lmod is special. For this we need to "spack load" this
+		# and then do additional environment manipulations to enable
+		# the heirachical modules.
+		# This looks for any module starting with "lmod" in case
+		# user wants to specify a specific version.  
+		if [[ $mod == lmod* ]] ; then
+			# Disable the non-heirarchical modules so we can enable
+			# heirarchical modules below.
+			spackarch=$(spack arch)	
+			spackgenarch=$(echo $spackarch | cut -d '-' -f1,2)-x86_64 # this is faster than: set spackgenarch=`spack arch -p`-`spack arch -o`-x86_64
 
-	#cmd="source modules_setup.sh"
-	#echo $cmd
-	#$cmd
+			# Setup to use heirarchical lmod modules from spack.
+			# n.b. At one point it seemed running "spack location"
+			# in a subprocess was adding more than a second to this
+			# script compared to the following:
+			#  set _lmod_loc = `echo $CMAKE_PREFIX_PATH | sed -e 's/:/\n/g' | grep lmod | head -n 1`
+			# Subsequent testing did not support that. I'm leaving this
+			# comment here in case we need to revisit using the above alternative
+			cmd="spack load $mod"
+			[[ $- == *i* ]] && echo $cmd
+			$cmd
+			#spack load $mod
+			_lmod_loc=$(spack location -i $mod)
+			source ${_lmod_loc}/lmod/lmod/init/bash
+			module unuse ${SPACK_ROOT}/share/spack/modules/${spackarch} ${SPACK_ROOT}/share/spack/modules/${spackgenarch}
+			module use ${SPACK_ROOT}/share/spack/lmod/${spackgenarch}/Core
+		else
+			
+			# Any modules other than lmod should be loaded with "module load"
+			cmd="module load $mod"
+			[[ $- == *i* ]] && echo $cmd
+			$cmd
 
-	# Optionally load the specified compiler
-	if [ "$#" -ge 3 ] ; then
-		cmd="module load $3"
-		echo $cmd
-		$cmd
-	fi
+		fi
+
+	done
+
 else
 	echo "No file: $setupfile"
 	echo "No spack config for os/release: ${os}/${ver}"
