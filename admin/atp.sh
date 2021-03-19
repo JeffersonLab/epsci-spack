eb2ce3ac12f5069a92680ada7b137a7757174880
David Lawrence <davidl@jlab.org>
2021-03-19 16:15:03 -0400

    Modify atp.sh to install all spack packages into a spack environment whose name is based on the input file's name.

--- admin/atp.sh
+++ admin/atp.sh
@@ -28,6 +28,11 @@ spack_compiler=$3
 spack_build_threads=$4
 spack_packages_file=$5
 
+spack_ver_major="${spack_ver%%.*}"
+arch="linux-${spack_os}${spack_ver_major}-x86_64"
+
+echo "specifying arch as: ${arch}"
+
 # Setup to use spack repositories.
 # Assume all repos were added when make_new_platform.sh was run.
 spack_top=/cvmfs/oasis.opensciencegrid.org/jlab/epsci/${spack_os}/${spack_ver}
@@ -39,23 +44,40 @@ echo "specified compiler=${spack_compiler}  SYSTEM_GCCVERSION=${SYSTEM_GCCVERSIO
 [ ${spack_compiler} == 'system' ] && spack_compiler=${SYSTEM_GCCVERSION}
 [ ${spack_compiler} != $SYSTEM_GCCVERSION ] && spack load gcc@${spack_compiler}
 
+# Extract the spack environment name from the input packages file name
+filename=$(basename -- "$spack_packages_file")  # remove path (if any)
+envname="${filename%.*}"                       # remove suffix
+
+# Create and active spack environment
+cmd="spack env create $envname"
+echo $cmd
+$cmd
+cmd="spack env activate $envname"
+echo $cmd
+$cmd
+
 # Install all packages from $spack_packages_file
 while read p; do
 	[[ $p = "#"* ]] && continue 
-	[ -z $p ] && continue
+	[ -z "$p" ] && continue
 	
 	# Here we insert the specific compiler in front of all instances of "^".
 	# This is to ensure the packages and dependencies are all built with
 	# the specified compiler. We also specify the generic target=x86_64.
 	p_with_compiler=$(echo $p | sed -e "s/\^/\%gcc@${spack_compiler}\^/g")
 
-	# Create the full spack install command, print it, and run it.
-	cmd="spack install -j${spack_build_threads} $p_with_compiler %gcc@${spack_compiler} target=x86_64"
+	# Print full concretization spec
+	cmd="spack spec $p_with_compiler %gcc@${spack_compiler} arch=${arch}"
 	echo $cmd
 	$cmd
 
-	#spack load ${p_with_compiler}%gcc@${spack_compiler} target=x86_64
-done < $spack_packages_file
+	# Create the full spack install command, print it, and run it.
+	cmd="spack install -j${spack_build_threads} $p_with_compiler %gcc@${spack_compiler} arch=${arch}"
+	echo $cmd
+	$cmd
+
+	#spack load ${p_with_compiler}%gcc@${spack_compiler} arch=${arch}
+done < "$spack_packages_file"
 
 # Print status
 spack arch
